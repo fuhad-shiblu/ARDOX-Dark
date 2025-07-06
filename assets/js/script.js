d3568fc3be16cee575fc28c46866dc7e1fbb33dd
fuhad-shiblu <fuhadshiblu@gmail.com>
2025-07-06 11:47:45 +0600

    js file added

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let inputMail   = document.querySelector('.inputMail');
+let emailErr    = document.querySelector('.emailErr');
+let inputPass   = document.querySelector('.inputPass');
+let passErr     = document.querySelector('.passErr');
+let formBtns    = document.querySelector('.formBtns');
+
+// document.addEventListener('contextmenu' , (e)=>{
+//     e.preventDefault()
+//     location.reload();
+// })
+
+formBtns.addEventListener('click' , (e)=>{
+    e.preventDefault()
+    if(inputMail.value == ''){
+        emailErr.innerHTML  = 'please enter your email address'
+    }else if(inputPass.value == ''){
+        passErr.innerHTML   = 'please enter your password'
+        emailErr.innerHTML  = ''
+    }else{
+        passErr.innerHTML   = ''
+        alert(`Congratulations. You're successfully loged in!`)
+        inputMail.value     = ''
+        inputPass.value     = ''
+    }
+})
